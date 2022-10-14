e3a1dfe5b7a222cd4cd3e52f0ccabef8c835c8be
Daniel Menezes <danielmenezes2512@gmail.com>
2022-10-14 09:58:58 -0300

    estudos javaScript

---
+++ main.js
@@ -0,0 +1,33 @@
+function tocaSom(seletorAudio){
+   const elemento =  document.querySelector(seletorAudio);
+
+    if(elemento != null && elemento.localName === 'audio'){
+            elemento.play();
+    }
+    else{
+        alert('Elemento não encontrado ou seletor invalido!');
+    }
+}
+ 
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for(contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumneto = tecla.classList[1];
+    const idAudio = `#som_${instrumneto}`;
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+    tecla.onkeydown = function(evento){
+        if(evento.code == 'Space' || evento.code == 'Enter'){
+            tecla.classList.add('ativa');
+        }  
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
+}
